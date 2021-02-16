768a750f5e2f6f56ddcc6617d7ddbbc447d45322
Asparuhov <krilmi220601@gmail.com>
2021-02-16 16:18:16 +0200

    making the chat box

--- client/src/containers/ChatPanel/ChatPanel.js
+++ client/src/containers/ChatPanel/ChatPanel.js
@@ -1,9 +1,20 @@
-import React from "react";
-import classes from './ChatPanel.module.css';
+import React, { useState } from "react";
+import classes from "./ChatPanel.module.css";
 const ChatPanel = (props) => {
+  let [message, setMessage] = useState("");
   return (
-  <div className={classes.chat}>
-      
-  </div>);
+    <div className={classes.chat}>
+      <div className={classes.Upper}></div>
+      <div className={classes.messagesBox}></div>
+      <div className={classes.bottom}>
+        <input
+          type="text"
+          placeholder="Type a message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 export default ChatPanel;
